3f13aad29557cdb20e6b442c6098f0446503dc3e
catalinwk <catalinwk@gmail.com>
2017-03-02 23:54:00 +0200

    ConcurentSorting - BubbleSort
    BubbleSort Phase

---
+++ concurentsorting/ConcurentSortingApp.java
@@ -0,0 +1,44 @@
+/*
+ * ConcurentSortingApp.java
+ */
+
+package concurentsorting;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ConcurentSortingApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ConcurentSortingView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ConcurentSortingApp
+     */
+    public static ConcurentSortingApp getApplication() {
+        return Application.getInstance(ConcurentSortingApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ConcurentSortingApp.class, args);
+    }
+}
